dc9f05f30299485c5a71e769e48fc9f9ea4550e8
ColinCarver10 <ccarver410@gmail.com>
2022-10-17 22:36:27 -0400

    Login screen design

--- OCKSample/Main/Login/LoginViewModel.swift
+++ OCKSample/Main/Login/LoginViewModel.swift
@@ -14,6 +14,8 @@ import CareKitStore
 import WatchConnectivity
 import os.log
 
+// swiftlint:disable function_parameter_count
+
 class LoginViewModel: ObservableObject {
 
     // MARK: Public read, private write properties
@@ -136,13 +138,15 @@ class LoginViewModel: ObservableObject {
      - parameter password: The password the person signing up.
      - parameter firstName: The first name of the person signing up.
      - parameter lastName: The last name of the person signing up.
+     - parameter email: The email of the person signing up.
     */
     @MainActor
     func signup(_ type: UserType,
                 username: String,
                 password: String,
                 firstName: String,
-                lastName: String) async {
+                lastName: String,
+                email: String) async {
         do {
             guard try await PCKUtility.isServerAvailable() else {
                 Logger.login.error("Server health is not \"ok\"")
@@ -152,6 +156,7 @@ class LoginViewModel: ObservableObject {
             // Set any properties you want saved on the user befor logging in.
             newUser.username = username.lowercased()
             newUser.password = password
+            newUser.email = email
             let user = try await newUser.signup()
             Logger.login.info("Parse signup successful: \(user)")
             let patient = try await savePatientAfterSignUp(type,
